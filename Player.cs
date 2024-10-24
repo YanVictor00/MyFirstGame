99c9ebfd2a9bdd67f67e43f400dd40d60b3dae16
Yan Victor <yanvictor799@gmail.com>
2024-10-24 19:25:42 -0300

    Explicando códigos 1

--- Player.cs
+++ Player.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//classe geral
 public class Player : MonoBehaviour
-{
+{//Variáveis
     Rigidbody2D rig;
     Animator anime;
     private bool isJumping;
@@ -11,46 +11,54 @@ public class Player : MonoBehaviour
     public float speed = 5;
     public float jumpForce;
 
-
+//método
     void Start()
     {
     rig = GetComponent<Rigidbody2D>();    
     anime = GetComponent<Animator>();
     }
-
+//método
     void Update()
-    {
+    {   //chamandoo método de movimentação e pulo.
         Move();
         Jump();
     }
-
+//método de movimentação (sistema de movimentação)
     void Move()
     {    
-        //se não pressionar nada valor é 0. Pressionar direita, valor máximo 1. Esquerda valor máximo -1.
+        //nova variável do tipo float recebe o Input.GetAxis( Mecânica de tecla de movimentação 2D )
         float movement = Input.GetAxis("Horizontal");
         Debug.Log(movement);
+
         // Adiciona velocidade ao corpo do personagem no eixo x e y.
         rig.velocity = new Vector2(movement * speed, rig.velocity.y);
 
+        //se a variável movement fot menor que zero ele vira o eixo Y a 180 graus.
         if(movement < 0)
         {
             transform.eulerAngles = new Vector3(0, 180f, 0);
         }
+
+        //se a variável movement fot maior que zero ele vira o eixo Y a 0 graus.
         if(movement > 0)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
     }
-
+//método de pulo ( sistema de pulo )
     void Jump()
-    {
+    {   
+        //Se precionar o botão Jump(Space)
         if(Input.GetButtonDown("Jump"))
         {   
-            //isJumping == false
+            //se isJumping == false
             if(!isJumping)
             {
+                //adionar uma força eixo Y no rig com impulso
                 rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                //se a tecla Space for pressionada
                 doubleJump = true;
+                //se a tecla Space for pressionada
                 isJumping = true;
             }
             else
