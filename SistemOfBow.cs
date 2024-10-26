9d3d6f3d09fd5ea06e20c5f6c384921ab8b1a73a
Yan Victor <yanvictor799@gmail.com>
2024-10-25 22:27:31 -0300

    Colocando descrição

--- SistemOfBow.cs
+++ SistemOfBow.cs
@@ -1,26 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//classe
 public class Bow : MonoBehaviour
 {
+//variáveis
     private Rigidbody2D rig;
     public float speed;
     public bool isRight;
+//método inicial
     void Start()
     {
+        //rig recebendo GetComponent
         rig = GetComponent<Rigidbody2D>();
+        //objeto se destruindo depois de 2 segundos
         Destroy(gameObject, 2f);
     }
-
+//método fps do rigidbody
     void FixedUpdate()
     {
+        //se isRight for verdadeiro
         if(isRight)
         {
+            //aplicar um vector 2 para direita * velocidade
             rig.velocity = Vector2.right * speed;
         }
         else
         {
+            //se isRight for false aplicar um vector 2 para esquerda * velocidade
             rig.velocity = Vector2.left * speed;
         }
        
