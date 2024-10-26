42f9021157186752f839f153d4ee70a9b496d269
Yan Victor <yanvictor799@gmail.com>
2024-10-25 22:30:48 -0300

    Depois faço a correção

--- CamDoPlayer.cs
+++ CamDoPlayer.cs
@@ -1,155 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//classe geral
-public class Player : MonoBehaviour
-{//Variáveis
-    Rigidbody2D rig;
-    Animator anime;
-    private float movement;
-    public GameObject bow;
-    public Transform firePoint;
-    private bool isJumping;
-    private bool doubleJump;
-    private bool isFire;
-    public float speed = 5;
-    public float jumpForce;
 
-//método inicial
+public class Cam : MonoBehaviour
+{
+    private Transform player;
+    public float smooth;
     void Start()
     {
-    //GetComponents
-    rig = GetComponent<Rigidbody2D>();    
-    anime = GetComponent<Animator>();
-    }
-//método frame por frame
-    void Update()
-    {   //chamandoo métodos.
-        Move();
-        Jump();
-        BowFire();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
-//método de movimentação (sistema de movimentação)
-    void Move()
-    {    
-        //variável recebe o Input.GetAxis( Mecânica de tecla de movimentação 2D )
-        movement = Input.GetAxis("Horizontal");
-        Debug.Log(movement);
-
-        // Adiciona velocidade ao corpo do personagem no eixo x e y.
-        rig.velocity = new Vector2(movement * speed, rig.velocity.y);
-
-        //se a variável movement fot menor que zero ele vira o eixo Y a 180 graus.
-        if(movement < 0)
-        {
-            //se isJumping for falso animar o player correndo
-            if(!isJumping)
-            {
-                anime.SetInteger("transition", 1);
-            }
-            //vira o player a 180 graus no eixo Y
-            transform.eulerAngles = new Vector3(0, 180f, 0);
-        }
-
-        //se a variável movement for maior que zero ele vira o eixo Y a 0 graus.
-        if(movement > 0)
-        {   
-            //se isJumping for falso animar o player correndo
-            if(!isJumping)
-            {
-                anime.SetInteger("transition", 1);
-            }
-            //vira o player a 0 graus no eixo Y (posição de fábrica)
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-
-        //se movement for igual a ZERO e isJumping for falso animar player pulando.
-        if(movement == 0 && !isJumping && !isFire)
-        {
-            anime.SetInteger("transition", 0);
-        }
-    }
-//método de pulo ( sistema de pulo )
-    void Jump()
+    void LateUpdate()
     {   
-        //Se precionar o botão Jump(Space)
-        if(Input.GetButtonDown("Jump"))
-        {   
-            //se isJumping == false
-            if(!isJumping)
-            {   
-                //animação de pulo
-                anime.SetInteger("transition", 2);
-                //adionar uma força eixo Y no rig com impulso
-                rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-                //se a tecla Space for pressionada
-                doubleJump = true;
-                //se a tecla Space for pressionada
-                isJumping = true;
-            }
-            //se não
-            else
-            {   //se pulo duplo for verdadeiro
-                if(doubleJump)
-                {   //animar
-                    anime.SetInteger("transition", 2);
-                    //aplicar mais uma força
-                    rig.AddForce(new Vector2(0, jumpForce * 1.2f), ForceMode2D.Impulse);
-                    //tornar pulo duplo false
-                    doubleJump = false;
-                }
-            }
-        }
-    }
 
-     //Método da habilade do ArcoFogo
-    void BowFire()
-    {
-        //chamando a corrotina
-       StartCoroutine(Fire());
-    }
-
-    //corrotina
-    private IEnumerator Fire()
-    {   
-        //se a tecla E for pressionado
-         if(Input.GetKeyDown(KeyCode.E))
-        {   
-            //isFire recebe True
-            isFire = true;
-            //animação 3 do transition é ativada
-            anime.SetInteger("transition", 3);
-            //instanciando uma váriavel local da flecha na posição e rotação do GameObject firePoint
-            GameObject Bow = Instantiate(bow, firePoint.position, firePoint.rotation);
-            //se o transform rotation Y for igual a 0, no caso o player virado para a direita
-            if(transform.rotation.y == 0)
-            {   
-                //acessar o script Bow e tornar a variável (isRight) True
-                Bow.GetComponent<Bow>().isRight = true;
-            }
-            //se o transform rotation Y for igual a 180, no caso o player virado para a esquerda
-            if(transform.rotation.y == 180)
-            {
-                //acessar o script Bow e tornar a variável (isRight) false
-                Bow.GetComponent<Bow>().isRight = false;
-            }
-
-            //depois de 1 segundo
-            yield return new WaitForSeconds(0.2f);
-            //voltar para animação 0 do transition (que é o "iddle")
-            anime.SetInteger("transition", 0);
-        }
-    }
-
-    //método de colisão com a camada Ground( Chão )
-    void OnCollisionEnter2D(Collision2D coll)
-    {   
-        //se gabeObject collidir com a camada 8
-        if(coll.gameObject.layer == 8)
+        if(player.position.x >= -1.5f && transform.position.x <= 40)
         {
-            //isJumping recebe false
-            isJumping = false;
+            Vector3 following = new Vector3(player.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime);
         }
+
     }
 }
