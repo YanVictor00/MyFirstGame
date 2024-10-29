73dc100f983603fcf28a35a1d9d23137eab98a49
Yan Victor <yanvictor799@gmail.com>
2024-10-28 21:21:15 -0300

    Colocando Health e Animação de Hit

--- EnemyGuyG.cs
+++ EnemyGuyG.cs
@@ -6,16 +6,20 @@ public class EnemyGuy : MonoBehaviour
 {
     public float speed;
     public float walkTime = 3f;
+
+    public int health;
+
     float timer;
     bool walkRight;
 
 
-
+    private Animator anim;
     private Rigidbody2D rig;
 
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -39,5 +43,16 @@ public class EnemyGuy : MonoBehaviour
             transform.eulerAngles = new Vector2(0,180f);
             rig.velocity = Vector2.right * speed; 
         }
+    }
+
+    public void Damage(int dmg)
+    {   
+        health -= dmg;
+        anim.SetTrigger("hit");
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
