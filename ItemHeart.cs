6429d91a88a7c91bd1b95390fa560a3ee0e8337d
Yan Victor <yanvictor799@gmail.com>
2024-11-29 22:50:03 -0300

    Descrição do script cura

--- ItemHeart.cs
+++ ItemHeart.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class ItemHeart : MonoBehaviour
 {
+    //Variavel inteira publica
     public int healthValue;
-
+// void de entrada de colisao 2D (Collisao2D collisao)
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //se colidir com um gameobject com a tag Player
         if(collision.gameObject.tag == "Player")
         {
-            collision.gameObject.GetComponent<Player>().IncreaseLife(healthValue);
+            //Collisao gameobject getcomponent player incrementar valor da variavel healthValue
+            collision.gameObject.GetComponent<Player>().IncreaseLife
+            (healthValue);
+            //destruir gameObejct
             Destroy(gameObject);
         }
     }
