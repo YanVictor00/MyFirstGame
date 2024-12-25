f829319d764f7c941e642d296c5a8c9a7ae4d31d
Yan Victor <yanvictor799@gmail.com>
2024-12-25 19:10:01 -0300

    Colocando Save do Score total

--- GameController.cs
+++ GameController.cs
@@ -10,11 +10,21 @@ public class GameController : MonoBehaviour
     //variável static
     public static GameController instance;
 
+    public int score;
+    public Text scoreText;
+    public int totalScore;
+
     //método chamado antes do Start na ordem de execução
     void Awake()
     {   
         //tornando static para acessar de outro script
         instance = this;
+    }
+
+    void Start()
+    {
+        totalScore = PlayerPrefs.GetInt("score");
+        Debug.Log(PlayerPrefs.GetInt("score"));
     }
 
     void Update()
@@ -23,11 +33,12 @@ public class GameController : MonoBehaviour
     }
 
     public void UpdateScore(int value)
-        {
-            score += value;
-            scoreText.text = score.ToString();
-        }
+    {
+        score += value;
+        scoreText.text = score.ToString();
 
+        PlayerPrefs.SetInt("score", score + totalScore);
+    }
 
     //método para passar o valor de vidas para o Text do Canva 
     public void UpdateLives(int value)
